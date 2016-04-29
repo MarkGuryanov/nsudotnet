ffe75cdbbbc53ef72d5a306e5c9653b4c555c156
Mark <mark-guryanov@yandex.ru>
2016-04-29 14:26:15 +0600

    Test commit

---
+++ Guryanov.Nsudotnet.NumberGuesser/Guryanov.Nsudotnet.NumberGuesser/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Guryanov.Nsudotnet.NumberGuesser
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, world!");
+
+            Console.ReadKey(false);
+        }
+    }
+}
